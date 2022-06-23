1643605428bcd6273b4426b224021cb6b2b6ce99
emersonli20 <61331273+emersonli20@users.noreply.github.com>
2022-06-23 10:36:34 -0400

    argparse and wrapped graphing functions

--- ribosome_density_from_membrane.py
+++ ribosome_density_from_membrane.py
@@ -15,38 +15,65 @@ def get_coordinates(filename: str) -> list[tuple[float, float, float],...]:
 
     return coordinates
 
+def graph_projection(coordinates: np.array, middle: int=150, interval: int=20):
+    x = coordinates[:,0]
+    y = coordinates[:,1]
+    z = coordinates[:,2]
+
+    lower = middle - interval
+    upper = middle + interval
+
+    condition = (z>lower) & (z<upper)
+    projection = coordinates[condition]
+    x_projection = projection[:, 0]
+    y_projection = projection[:, 1]
+    plt.scatter(x_projection,y_projection)
+    plt.title("Membrane Projection")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
+
+
+def graph_3d(x: np.array, y: np.array, z: np.array):
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+    ax.scatter3D(x, y, z, s=10)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+    plt.title("Membrane 3d")
+    plt.show()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-p","--projection", help="Display projection of tomogram onto the yz plane", type=bool)
+    parser.add_argument("-p","--projection", help="Display projection of tomogram onto the yz plane", action="store_true")
     parser.add_argument("--filename", help="Name of the json file containing the membrane coordinates", type=str, required=True)
+    parser.add_argument("--middle_slice", help="Middle slice when looking at projection", type=int, default=150)
+    parser.add_argument("--interval", help="Interval distance from middle slice when looking at projection", type=int, default=20)
+
     args = parser.parse_args()
 
     projection = args.projection
     filename = args.filename
+    middle_slice = args.middle_slice
+    interval = args.interval
 
     coordinate_list = get_coordinates(filename)
     coordinates_np = np.array(coordinate_list)
 
-    d1 = coordinates_np[:, 0]
-    d2 = coordinates_np[:, 1]
     theta = coordinates_np[:, 2]
+    d1 = coordinates_np[:, 0] - 341 * np.sin(np.deg2rad(theta))
+    d2 = coordinates_np[:, 1] - 480
 
-    x = d1 * np.cos(np.deg2rad(theta))
-    y = d1 * np.sin(np.deg2rad(theta))
-    z = d2
+    z = d1 * np.cos(np.deg2rad(theta))
+    x = d1 * np.sin(np.deg2rad(theta))
+    y = d2
+
+    cartesian_coordinates = np.hstack([x[:,None],y[:,None],z[:,None]])
 
     if (projection):
-        condition = (z>70) & (z<130)
-        projection = coordinates_np[condition]
-        condition = (z>70) & (z<130)
-        projection = coordinates_np[condition]
-        x_projection = projection[:, 0]
-        y_projection = projection[:, 1]
-        plt.scatter(x_projection,y_projection)
+        graph_projection(cartesian_coordinates, middle_slice, interval)
     else:
-        fig = plt.figure()
-        ax = plt.axes(projection="3d")
-        ax.scatter3D(x, y, z, s=10)
+        graph_3d(x,y,z)
 
-    plt.show()
