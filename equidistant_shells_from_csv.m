9471c105135493f80c1b03f25708bfa83c04d87e
eliselyhan <97372314+eliselyhan@users.noreply.github.com>
2022-07-07 11:40:34 -0400

    show_normals

--- equidistant_shells_from_csv.m
+++ equidistant_shells_from_csv.m
@@ -1,30 +1,28 @@
-all_coords = csvread("coordinates_3.0.csv");
-mem = csvread("single_mem_coords.csv");
+% all_coords = csvread("coordinates_3.0.csv");
+mem = csvread("inner_mem_coords.csv");
 
 % generate original pointcloud and normals
 ptcloud = pointCloud(mem);
 normals = pcnormals(ptcloud, 120);
-adjusted_normals = get_adjusted_normals(ptcloud, 1,120);
+adjusted_normals = get_adjusted_normals(ptcloud, 5,120);
 n = numel(adjusted_normals)/3
 
 % manually flip the rest of the upward-facing normals
 adjusted_normals = manually_flip_y(adjusted_normals);
-
+show_normals(ptcloud, 5, adjusted_normals)
 % write final normals to csv
 csvwrite("adjusted_normals.csv", adjusted_normals);
 
 % generate shells, write to csv
-shells = make_shells(mem, adjusted_normals, 50);
-csvwrite("shells_coords.csv", shells);
+shell_number = 150
+shells = make_shells(mem, adjusted_normals, shell_number);
+filename  = sprintf('shells_coords_%d.csv',shell_number)
+csvwrite(filename,  shells);
 
 hold on
-pcshow(ptcloud);
+% pcshow(ptcloud);
 
-% title('Adjusted Normals of Point Cloud')
-% quiver3(x_p, y_p, z_p, u, v, w);
-
-% visualization of shells
-% hold on
+% % visualization of shells
 % shell_10 = shells(:,:,10);
 % shell_20 = shells(:,:,20);
 % shell_30 = shells(:,:,30);
@@ -38,10 +36,5 @@ pcshow(ptcloud);
 % ylabel('y')
 % zlabel('z')
 % view([0,0,180])
-% 
 % hold off
 % 
-% 
-
-
-
