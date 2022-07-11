3b44f1ca53a4e79f96cb50cc7ffc572e9a245231
eliselyhan <97372314+eliselyhan@users.noreply.github.com>
2022-07-11 12:15:51 -0400

    brush to select desired membranes

---
+++ brushing.m
@@ -0,0 +1,13 @@
+% all_coords = csvread("/datadisk/cmholab3/tomography/20220120_5970-3_trophs/L5/eman2/ts001/inner_mem_1_coords.csv");
+
+plot3(all_coords(:,1), all_coords(:,2), all_coords(:,3),'.')
+
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+view([0,0,180])
+
+% path = insert_desired_output_path_of_selected_membrane
+
+% csvwrite("/datadisk/cmholab3/tomography/*path", brushedData)
