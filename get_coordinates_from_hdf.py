f2d55b3ee41754b2cfa88212f680ff0eca33889d
emersonli20 <61331273+emersonli20@users.noreply.github.com>
2022-07-12 17:31:17 -0400

    pass z range as arg in get_coordinates_from_hdf.py

--- get_coordinates_from_hdf.py
+++ get_coordinates_from_hdf.py
@@ -1,20 +1,19 @@
 import pandas as pd
-import tables
 import numpy as np
 import h5py
 import argparse
 import json
 
-def get_coordinates(filename: str, threshold: float=5):
+def get_coordinates(filename: str, z: int, threshold: float=5):
     f = h5py.File(filename,'r')
     m = f['MDF']
     #PRINT(f.keys())
-    dataset = m['images']['0']['image']
+    dataset = m['images']['0']['image']  # type: ignore
     arr = np.array(dataset)
 
-    arr = arr.reshape(352,-1)
+    arr = arr.reshape(z,-1)
     df = pd.DataFrame(arr)
-    non_zero_indices = df[df>thresh].stack().index.tolist()
+    non_zero_indices = df[df>threshold].stack().index.tolist()
     non_zero_coords = []
     for value in non_zero_indices:
         z = value[0]
@@ -29,27 +28,30 @@ if __name__ == "__main__":
 
     parser.add_argument("-f","--filename", type=str, help="name of the segmentation with h5 extension", required = True)
     parser.add_argument("-t","--threshold", type=float, help="threshold of color intensity, e.g.5", required = True)
+    parser.add_argument("-z","--z_range", type=int, help="z range of the tomogram", required = True)
     args = parser.parse_args()
 
     filename = args.filename
-    thresh = args.threshold
+    threshold = args.threshold
+    z = args.z_range
 
-    f = h5py.File(filename,'r')
-    m = f['MDF']
-    #PRINT(f.keys())
-    dataset = m['images']['0']['image']
-    arr = np.array(dataset)
+    # f = h5py.File(filename,'r')
+    # m = f['MDF']
+    # #PRINT(f.keys())
+    # dataset = m['images']['0']['image']
+    # arr = np.array(dataset)
 
-    arr = arr.reshape(352,-1)
-    df = pd.DataFrame(arr)
-    non_zero_indices = df[df>thresh].stack().index.tolist()
-    non_zero_coords = []
-    for value in non_zero_indices:
-        z = value[0]
-        y = value[1] // 686
-        x = value[1] - y * 686
-        non_zero_coords.append((x,y,z))
+    # arr = arr.reshape(z,-1)
+    # df = pd.DataFrame(arr)
+    # non_zero_indices = df[df>threshold].stack().index.tolist()
+    # non_zero_coords = []
+    # for value in non_zero_indices:
+    #     z = value[0]
+    #     y = value[1] // 686
+    #     x = value[1] - y * 686
+    #     non_zero_coords.append((x,y,z))
 
+    non_zero_coords = get_coordinates(filename, z, threshold)
 
     jsonString = json.dumps(non_zero_coords)
     jsonFile = open("coords.json","w")
