73267a9a4a309a12a6454e3c217c31c2406cc7e4
eliselyhan <97372314+eliselyhan@users.noreply.github.com>
2022-07-13 12:22:24 -0400

    Update equidistant_shells_from_csv.m

--- equidistant_shells_from_csv.m
+++ equidistant_shells_from_csv.m
@@ -1,11 +1,13 @@
 % all_coords = csvread("coordinates_3.0.csv");
 mem = csvread("/path of pv/pvm/dv membrane coords.csv");
-
 % generate original pointcloud and normals
-compress_ratio = 3
+compress_ratio = 3;
+neighbors = 400;
+shell_number = 150;
+
 mem_compressed = mem(1:compress_ratio:end, 1:3);
 ptcloud = pointCloud(mem_compressed);
-normals = pcnormals(ptcloud, 400);
+normals = pcnormals(ptcloud, neighbors);
 adjusted_normals = get_adjusted_normals(ptcloud, normals);
 n = numel(adjusted_normals)/3 %number of mem points selected
 % OPTIONAL step, uncomment if needed: manually flip the rest of the upward-facing normals
@@ -13,7 +15,6 @@ n = numel(adjusted_normals)/3 %number of mem points selected
 show_normals(ptcloud, adjusted_normals)
 % csvwrite("adjusted_normals.csv", adjusted_normals);
 % generate shells, write to csv
-shell_number = 150
 shells = make_shells(mem_compressed, adjusted_normals, shell_number);
 filename  = sprintf('/path of output pm/pvm/dv_shells_coords_%d.csv',shell_number)
 csvwrite(filename,  shells);
