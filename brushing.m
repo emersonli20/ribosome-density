acb9fd129fe0ace5ed895aaffa5b1024988bb266
eliselyhan <97372314+eliselyhan@users.noreply.github.com>
2022-07-13 13:03:28 -0400

    Update brushing.m

--- brushing.m
+++ brushing.m
@@ -1,4 +1,4 @@
-all_coords = csvread("/datadisk/cmholab3/tomography/20220120_5970-3_trophs/L5/eman2/ts001/inner_mem_1_coords.csv");
+% all_coords = csvread("/datadisk/cmholab3/tomography/20220120_5970-3_trophs/L5/eman2/ts001/inner_mem_1_coords.csv");
 
 plot3(all_coords(:,1), all_coords(:,2), all_coords(:,3),'.')
 
@@ -8,7 +8,5 @@ zlabel('z')
 
 view([0,0,180])
 axis equal
-
-% path = insert_desired_output_path_of_selected_membrane
 
 % csvwrite("/datadisk/cmholab3/tomography/*path", brushedData)
