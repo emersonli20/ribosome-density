ae332c9f61f6e9a51e352324dfa953bbcf2a4953
eliselyhan <97372314+eliselyhan@users.noreply.github.com>
2022-08-10 08:15:44 -0700

    Update get_coordinates_from_hdf.py

--- get_coordinates_from_hdf.py
+++ get_coordinates_from_hdf.py
@@ -5,7 +5,7 @@ import argparse
 import json
 import os
 
-def get_coordinates(filename: str, z: int, threshold: float=5):
+def get_coordinates(filename: str, x: int, z: int, threshold: float=5):
     f = h5py.File(filename,'r')
     m = f['MDF']
     #PRINT(f.keys())
@@ -18,8 +18,8 @@ def get_coordinates(filename: str, z: int, threshold: float=5):
     x_list, y_list, z_list = [], [], []
     for value in membrane_indices:
         z = value[0]
-        y = value[1] // 686
-        x = value[1] - y * 686
+        y = value[1] // x
+        x = value[1] - y * x
         membrane_coords.append((x,y,z))
 
     return membrane_coords
@@ -36,12 +36,14 @@ if __name__ == "__main__":
 
     parser.add_argument("-f","--filename", type=str, help="name of the segmentation with h5 extension", required = True)
     parser.add_argument("-t","--threshold", type=float, help="threshold of color intensity, e.g.5", required = True)
+    parser.add_argument("-x","--x_range", type=int, help="x range of the tomogram", required = True)
     parser.add_argument("-z","--z_range", type=int, help="z range of the tomogram", required = True)
     args = parser.parse_args()
 
     filename = args.filename
     threshold = args.threshold
+    x = args.x_range
     z = args.z_range
-    membrane_coords = get_coordinates(filename, z, threshold)
+    membrane_coords = get_coordinates(filename, x, z, threshold)
 
     list_to_csv(membrane_coords, filename)
