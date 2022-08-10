80614c4e2178586909ac7cbff3665835992b5a9b
emersonli20 <61331273+emersonli20@users.noreply.github.com>
2022-08-10 14:49:06 -0400

    Update csv_to_hdf.py

--- csv_to_hdf.py
+++ csv_to_hdf.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-f", "--filename", type=str, required=True)
+parser.add_argument("-f", "--filename", type=str, required=True, help="path of filled membrane csv file from fill_grid.py")
 parser.add_argument("-o", "--output", type=str, required=True)
 parser.add_argument("-x", "--x_dim", type=int, required=True)
 
@@ -18,7 +18,16 @@ arr = np.loadtxt(filename, delimiter=",")
 arr = arr.reshape(arr.shape[0], x_dim, -1)
 arr = arr.transpose(0,2,1)
 
-print(arr.shape)
+# ellipse membrane
+# arr = arr.transpose(2,1,0)
+# print(arr.shape)
+
+#d = np.zeros((256,960,686))
+#indices = np.loadtxt(filename, delimiter = ',')
+#for i in range(indices.shape[0]):
+#	line = indices[i, :]
+#	print(line.shape)
+#	d[int(line[2]), int(line[1]), int(line[0])]=4
 
 h5f = h5py.File(output, 'w')
 h5f.create_dataset('dataset_1', data = arr)
